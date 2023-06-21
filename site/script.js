405e130579961bb7b7a989ca4f83a72520ebeaae
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-06-20 20:32:04 -0700

    Many borders, plus text comes from above now

--- site/script.js
+++ site/script.js
@@ -1,12 +1,12 @@
 function glideTitle() {
   const title = document.getElementById('gliding-title');
   title.style.opacity = '0';
-  title.style.transform = 'translateX(-100%)';
+  title.style.transform = 'translateY(-200%)';
 
   setTimeout(() => {
     title.style.transition = 'opacity 1s, transform 1s';
     title.style.opacity = '1';
-    title.style.transform = 'translateX(0)';
+    title.style.transform = 'translateY(0)';
   }, 100); //Slight delay
 }
 
