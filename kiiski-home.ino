bd1dea0ccdb3430ddaaaf88b0c6eb5d503809e8a
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-06-25 20:59:34 -0700

    Moved back to individual files

--- kiiski-home.ino
+++ kiiski-home.ino
@@ -36,31 +36,52 @@ void loop() {
         char c = client.read(); 
 
         request += c;
-        Serial.print(c);
 
         if (c == '\n'){
-          Serial.println(request);
           int indx = request.indexOf("GET /");
           
           //GET Request
           if (indx >= 0){
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-Type: text/html");
-            client.println();
+            String fileName;
 
-            //Send index.htm to client
-            File file = SD.open("index.htm");
+            //Looking for script.js
+            if (request.indexOf("script.js") > 0){
+              fileName = "script.js";
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/javascript");
+              client.println();
+
+            //Looking for style.css
+            } else if (request.indexOf("style.css") > 0){
+              fileName = "style.css";
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/css");
+              client.println();
+
+            //Looking for index.htm
+            } else {
+              fileName = "index.htm";
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: text/html");
+              client.println();
+            }
+
+            //Send client
+            Serial.print("FILENAME: ");
+            Serial.println(fileName);
+            File file = SD.open(fileName);
             if (file){
-              
-                while (file.available()){
-                  client.write(file.read());
-                }
+              while (file.available()){
+                client.write(file.read());
+              }
               file.close();
 
+            } else {
+              Serial.print(fileName);
+              Serial.println(" does NOT exist!");
             }
             break;
           }
-          request = "";
         }
       }
     } 
